351dff5c2c6409d34f4eb5e24f4db2f9497b6e3d
Florent Falcy <flow38@gmail.com>
2016-03-18 17:27:50 +0100

    New IService interface

---
+++ VisualStudioProject/impl/AbstractService.cs
@@ -0,0 +1,11 @@
+﻿
+using strange.extensions.hollywood.api;
+
+namespace strange.extensions.hollywood.impl
+{
+    public abstract class AbstractService
+    {
+        [Inject]
+        public IStartDirectorsSignal StartDirectors { get; set; }
+    }
+}
