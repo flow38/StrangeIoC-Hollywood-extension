53e8e914f83c840e7b933b87d5ff9865a199e73c
Florent Falcy <flow38@gmail.com>
2016-05-06 10:05:07 +0200

    Sugar interface for basic Signal instance

---
+++ VisualStudioProject/api/ISignal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using strange.extensions.signal.api;
+
+namespace strange.extensions.hollywood.api
+{
+    public interface ISignal : IBaseSignal
+    {
+        void Dispatch();
+
+        void AddListener(Action callback);
+
+        void RemoveListener(Action callback);
+
+        void AddOnce(Action callback);
+    }
+}
