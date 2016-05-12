fe50d2bb00fcd2b38da5551853889ce5d4cc727f
Florent Falcy <flow38@gmail.com>
2016-05-12 13:02:19 +0200

    Add a base class for Signal which not dispatch any arguments

---
+++ VisualStudioProject/impl/SimpleSignal.cs
@@ -0,0 +1,10 @@
+﻿using strange.extensions.hollywood.api;
+using strange.extensions.signal.impl;
+
+namespace strange.extensions.hollywood.impl
+{
+    public class SimpleSignal : Signal, ISignal
+    {
+
+    }
+}
