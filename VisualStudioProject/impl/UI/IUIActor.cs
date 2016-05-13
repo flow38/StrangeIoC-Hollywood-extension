d0051edacb016857086f52054d9f3636d29e7b88
Florent Falcy <flow38@gmail.com>
2016-05-13 15:07:08 +0200

    IUIActor
    Ad important helper fonction to handle UI element size & position
    (TransformRect)

--- VisualStudioProject/impl/UI/IUIActor.cs
+++ VisualStudioProject/impl/UI/IUIActor.cs
@@ -6,5 +6,29 @@ namespace strange.extensions.hollywood.impl.UI
     public interface IUIActor : IActor
     {
         RectTransform RectTransform { get; }
+
+        /// <summary>
+        /// Set normalised Actor position relative to parent pivot point
+        /// </summary>
+        /// <param name="position">Percent Actor position relative to parent pivot point<</param>
+        void SetRectTransformPosition(Vector2 position);
+
+        /// <summary>
+        /// Get normalised Actor position relative to parent pivot point
+        /// </summary>
+        /// <returns>Percent Actor position relative to parent pivot point</returns>
+        Vector2 GetRectTransformPosition();
+
+        /// <summary>
+        /// Set normalised Actor size relative to parent size
+        /// </summary>
+        /// <param name="size">Percent Actor size relative to parent size</param>
+        void SetRectTransformSize(Vector2 size);
+
+        /// <summary>
+        ///  </summary>
+        /// <returns>Percent Actor size relative to parent size</returns>
+        Vector2 GetRectTransformSize();
+
     }
 }
--- VisualStudioProject/impl/UI/UIActor.cs
+++ VisualStudioProject/impl/UI/UIActor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace strange.extensions.hollywood.impl.UI
 {
@@ -6,17 +7,86 @@ namespace strange.extensions.hollywood.impl.UI
     public abstract class UIActor : Actor, IUIActor
     {
         private RectTransform _rectTransform;
+        private RectTransform _parentRectTransform;
 
         public RectTransform RectTransform
         {
             get { return _rectTransform; }
         }
 
+        public void SetRectTransformPosition(Vector2 position)
+        {
+            //Convert percent values to pixel values
+            if (_parentRectTransform.rect.width != 0 && _parentRectTransform.rect.height != 0)
+            {
+                position.x = position.x * _parentRectTransform.rect.width;
+                position.y = position.y * _parentRectTransform.rect.height;
+            }
+            else
+            {
+                position.x = position.x * Screen.width;
+                position.y = position.y * Screen.height;
+            }
+
+            _rectTransform.anchoredPosition = position;
+        }
+
+        public Vector2 GetRectTransformPosition()
+        {
+            Vector2 position = _rectTransform.anchoredPosition;
+            if (_parentRectTransform.rect.width != 0 && _parentRectTransform.rect.height != 0)
+            {
+                position.x = position.x / _parentRectTransform.rect.width;
+                position.y = position.y / _parentRectTransform.rect.height;
+            }
+            else
+            {
+                position.x = position.x / Screen.width;
+                position.y = position.y / Screen.height;
+            }
+
+            return position;
+        }
+
+        public void SetRectTransformSize(Vector2 size)
+        {
+            if (size.x < 0 || size.y < 0)
+            {
+                throw (new Exception("size vector x & y properties cannot be negative (mean nothing about a width & height percent valeur)"));
+            }
+
+            //Convert percent values to pixel values
+            if (_parentRectTransform.rect.width != 0 && _parentRectTransform.rect.height != 0)
+            {
+                size.x = _parentRectTransform.rect.width * size.x - _parentRectTransform.rect.width;
+                size.y = _parentRectTransform.rect.height * size.y - _parentRectTransform.rect.height;
+            }
+            else
+            {
+                size.x = Screen.width * size.x - Screen.width;
+                size.y = Screen.height * size.y - Screen.height;
+            }
+
+            _rectTransform.sizeDelta = size;
+        }
+
+        public Vector2 GetRectTransformSize()
+        {
+            Vector2 size = _rectTransform.sizeDelta;
+            size.x = size.x / Screen.width;
+            size.y = size.y / Screen.height;
+
+            return size;
+        }
+
+
         protected override void Awake()
         {
             _rectTransform = GetComponent<RectTransform>();
+            _parentRectTransform = transform.parent.GetComponentInParent<RectTransform>();
 
             base.Awake();
         }
+
     }
 }
