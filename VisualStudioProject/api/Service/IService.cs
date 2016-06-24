ba270f039f89b8ed6708bb206cfdf8765f1a759b
Florent Falcy <flow38@gmail.com>
2016-06-24 12:19:32 +0200

    Introduce IService & HollywoodService
    First service is a Timer handler

---
+++ VisualStudioProject/api/Service/IService.cs
@@ -0,0 +1,23 @@
+﻿using strange.extensions.signal.api;
+
+namespace strange.extensions.hollywood.api
+{
+    /// <summary>
+    /// IService is a Service Class as Domain Driven Developement define it :
+    /// - "When an operation does not conceptually belong to any object. Following the natural contours of the problem, you can implement these operations in services." (Wikipedia)
+    /// - https://en.wikipedia.org/wiki/Domain-driven_design
+    /// 
+    /// In Unity context, IService is NEVER a MonoBehavior subclass.
+    /// If a service need to use Unity API, you must create an Actor & Director instead or inject to the service all needed Director that
+    /// will perform via their Actors the Unnity API calls.
+    /// </summary>
+    public interface IService
+    {
+        /// This method is fires when ContextView get its Start() Monobehavior event
+        /// Here, you're sur that all Hollywood actor, Directors and Service exist.
+        /// 
+        /// ASync injection : 
+        /// This method fit particularly to get reference to other Class via Strange injection binder
+        void OnContextStart(IBaseSignal baseSignal, object[] objects);
+    }
+}
